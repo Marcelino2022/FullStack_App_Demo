9479402101feac010624c4ee384136ea70a8e524
Manuel Marcelino <101073815+Marcelino2022@users.noreply.github.com>
2022-09-29 18:05:29 +0100

    Delete app directory

--- app/Repositories/CoordenacaoRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace App\Repositories;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Http\Request;
-
-    class CoordenacaoRepository extends AbstractRepository{
-        
-    }
-?>
--- app/Repositories/MembroRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\Repositories;
-    use Illuminate\Database\Eloquent\Model;
-
-    class MembroRepository extends AbstractRepository{
-
-    }
-?>
